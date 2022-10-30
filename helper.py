ae025b2c3ca3ffd0f94f29a4d3447bb5413955d3
Stephen Metcalfe <stephen.metcalfe@prodigyfinance.com>
2022-10-30 21:09:26 +0200

    Getting around relative paths - essential for cron jobs

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import configparser
-
+import os
 def read_config():
     config = configparser.ConfigParser()
-    config.read('settings.ini')
+    config.read(os.path.join(os.path.dirname(__file__), 'settings.ini'))
     return config
